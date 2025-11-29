58e145f27c5cf02cac741da5c072d398dd1ef372
JadedMystery <jadedscuteclips@gmail.com>
2025-11-29 11:45:39 -0600

    Add files via upload

--- assets/js/camera.js
+++ assets/js/camera.js
@@ -1,17 +1,17 @@
 
 const scenes=[
- 'assets/img/camera/scene_1.png',
- 'assets/img/camera/scene_2.png',
- 'assets/img/camera/scene_3.png',
- 'assets/img/camera/scene_4.png',
- 'assets/img/camera/scene_5.png',
- 'assets/img/camera/scene_6.png'
+  "assets/img/camera/scene_1.png",
+  "assets/img/camera/scene_2.png",
+  "assets/img/camera/scene_3.png",
+  "assets/img/camera/scene_4.png",
+  "assets/img/camera/scene_5.png",
+  "assets/img/camera/scene_6.png"
 ];
 let idx=0;
 setInterval(()=>{
- idx=(idx+1)%scenes.length;
- let L=document.getElementById('cctvLobby');
- if(L) L.src=scenes[idx];
- let V=document.getElementById('cctvVan');
- if(V) V.src=scenes[idx];
+  idx=(idx+1)%scenes.length;
+  let l=document.getElementById("cctvLobby");
+  if(l) l.src=scenes[idx];
+  let v=document.getElementById("cctvVan");
+  if(v) v.src=scenes[idx];
 },5000);
