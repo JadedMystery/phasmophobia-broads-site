dc1312fa19bacbc082084f84ec23e4a132228938
JadedMystery <jadedscuteclips@gmail.com>
2025-11-29 19:44:05 -0600

    Add files via upload

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,5 +1,7 @@
 
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyB2WA7yotRlqNidwIgJcT19JNrK8ukMgs4",
   authDomain: "phasmophobiabroads.firebaseapp.com",
@@ -8,4 +10,6 @@ const firebaseConfig = {
   messagingSenderId: "503659624108",
   appId: "1:503659624108:web:6e57fbc6bf36b0d5989109"
 };
-const app = initializeApp(firebaseConfig);
+
+export const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
